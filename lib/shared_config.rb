e3e0d32181f6640e35463432837396d748bc1873
Erich L Timkar <erich@teamdriveaway.com>
2019-09-05 10:16:46 -0500

    Adds support for loading all.

--- lib/shared_config.rb
+++ lib/shared_config.rb
@@ -6,8 +6,16 @@ module SharedConfig
   class S3
     class << self
       def load(*group_names)
-        groups = ObjectKeys.from_group_names(*group_names).map{|object_key| SharedConfig::Group.new(object_key: object_key)}
+
+        if (group_names = ObjectKeys.from_group_names(*group_names)).any?
+          groups = group_names.map{|object_key|
+            SharedConfig::Group.new(object_key: object_key)
+          }
+        else
+          groups = SharedConfig::Group.all
+        end
         groups.each(&:load)
+        return "#{groups.length} groups loaded"
       end
     end
   end
@@ -34,52 +42,48 @@ module SharedConfig
   end
 
   class Group
-    attr_reader :object_key, :object
-    def initialize(object_key:, object: nil)
+    attr_reader :object_key, :object_summary
+    def initialize(object_key: nil, object_summary: nil)
       @object_key=object_key
-      @object = object
-      self.load_object unless object
+      @object_summary = object_summary
+      self.load_object_summary unless object_summary
     end
 
     def load
-      self.load_json(self.object).each{|key, value|
+      puts "Loading Shared Config - #{self.object_key}" if ENV['DEBUG_SHARED_CONFIG']
+      self.load_json(self.object_summary).each{|key, value|
         EnvVar.set(key, value)
       }
     end
 
-    def load_object
-      @object=self.bucket.objects[self.object_key]
+    def load_object_summary
+      @object_summary=Aws::S3::ObjectSummary.new(self.class.bucket.name, self.object_key)
     end
 
-    def load_json(object)
-      string = object.body.read
+    def load_json(object_summary)
+      string = object_summary.get.body.read
       return {} if string.nil? || string.empty?
       return  JSON.parse(string)
     end
-
-    def client
-      return Aws::S3::Client.new
-    end
-
 
     class << self
       def bucket
         return Aws::S3::Bucket.new(:name=>ENV['S3_CONFIG_BUCKET'], :client=>Aws::S3::Client.new)
       end
 
-      def all
-        objects = []
+      def all_object_summaries
+        object_summaries = []
         self.bucket.objects(prefix: ENV['SHARED_CONFIG_PATH_ROOT']).each{|os|
           if os.key =~ /\.json$/
-            objects << os.get
+            object_summaries << os
           end
         }
-        return objects
+        return object_summaries
       end
-    end
 
-    def all_objects_regexp
-      Regexp.new('^'+ENV['SHARED_CONFIG_PATH_ROOT']+'.*\.json$')
+      def all
+        self.all_object_summaries.map{|os| new(object_summary: os, object_key: os.key)}
+      end
     end
   end
 end
--- spec/shared_config_spec.rb
+++ spec/shared_config_spec.rb
@@ -1,124 +1,122 @@
 require "spec_helper"
 RSpec.describe SharedConfig do
-  let(:object){Aws::S3::Object.new(bucket_name: ENV['S3_CONFIG_BUCKET'], key: "#{ENV['SHARED_CONFIG_PATH_ROOT']}/#{rand_string}.json")}
+  let(:object_summary){Aws::S3::ObjectSummary.new(ENV['S3_CONFIG_BUCKET'], "#{ENV['SHARED_CONFIG_PATH_ROOT']}/#{rand_string}.json")}
   let(:group_name){SecureRandom.hex(12)}
   let(:object_key){"s3://#{SecureRandom.hex}"}
   let(:client){Aws::S3::Client.new}
   let(:bucket){Aws::S3::Bucket.new(name: ENV['S3_CONFIG_BUCKET'], client: client)}
 
   describe SharedConfig::S3 do
-    # let(:group){SharedConfig::Group.new(group_name)}
-
+    let(:group){SharedConfig::Group.new(object_key: object_key)}
     it "calls create a group with supplied " do
-      name = rand_string
-      object_key = rand_string
-      group = SharedConfig::Group.new(object_key: rand_string, object: object)
-
-      expect(SharedConfig::ObjectKeys).to receive(:from_group_names).with(name){[object_key]}
+      group
+      expect(SharedConfig::ObjectKeys).to receive(:from_group_names).with(group_name){[object_key]}
       expect(SharedConfig::Group).to receive(:new).with(object_key: object_key){group}
       expect(group).to receive(:load)
-      described_class.load(name)
-    end
-    #
-    # it "calls load on each item in SharedConfig::Group.all" do
-    #   expect(SharedConfig::Group).to receive(:all){[group]}
-    #   expect(group).to receive(:load)
-    #   described_class.load
-    # end
-
-    describe SharedConfig::ObjectKeys do
-      describe "from_group_names" do
-        it "calls from_group_name for each item supplied" do
-          names = %w{some-name some-other}
-          expect(described_class).to receive(:from_group_name).with('some-name')
-          expect(described_class).to receive(:from_group_name).with('some-other')
-          described_class.from_group_names(names)
-
-          expect(described_class).to receive(:from_group_name).with('some-name')
-          expect(described_class).to receive(:from_group_name).with('some-other')
-          described_class.from_group_names(*names)
-        end
-      end
-      describe "from_group_name" do
-        it "combines the the name with ENV['SHARED_CONFIG_PATH_ROOT']" do
-          expect(described_class.from_group_name('some-group-name')).to eq("#{ENV['SHARED_CONFIG_PATH_ROOT']}/some-group-name.json")
-          expect(described_class.from_group_name('some-group-name.json')).to eq("#{ENV['SHARED_CONFIG_PATH_ROOT']}/some-group-name.json")
-        end
-      end
-    end
-    describe SharedConfig::EnvVar do
-      describe "set" do
-        it "sets an environment variable" do
-          key = rand_string
-          value = rand_string
-          described_class.set(key, value)
-          expect(ENV[key]).to eq(value)
-        end
-      end
+      described_class.load(group_name)
     end
 
-    describe SharedConfig::Group do
-      subject{described_class.new(object_key: rand_string, object: object)}
-
-      describe "load" do
-        it "calls EnvVar.set with item from load_json" do
-          expect(subject).to receive(:load_json).with(subject.object){{"key"=>"value"}}
-          expect(SharedConfig::EnvVar).to receive(:set).with("key", "value")
-          subject.load
-        end
-      end
-
-      describe "load_json" do
-        let(:body){double("Body")}
-
-        it "returns a parsed version of the object's content" do
-          expect(object).to receive(:body){body}
-          expect(body).to receive(:read){"\[1\]"}
-          expect(subject.load_json(object)).to eq([1])
-        end
-
-        it "does not blow up if the json is unparseable" do
-          expect(object).to receive(:body){body}
-          expect(body).to receive(:read){""}
-          expect(subject.load_json(object)).to eq({})
-        end
-
-        it "does not blow up if the returned value is nil" do
-          expect(object).to receive(:body){body}
-          expect(body).to receive(:read){nil}
-          expect(subject.load_json(object)).to eq({})
-        end
-      end
-
-      describe "load_object" do
-        it "loads the object wth the env bucket" do
-          expect(subject).to receive(:bucket){bucket}
-          expect(bucket).to receive(:objects){{subject.object_key=>object}}
-          subject.load_object
-          expect(subject.object).to eq(object)
-        end
-      end
-
-      describe "all_objects_regexp" do
-        it "includes ENV['SHARED_CONFIG_PATH_ROOT']" do
-          expect(subject.all_objects_regexp.to_s).to include(ENV['SHARED_CONFIG_PATH_ROOT'])
-        end
-      end
-
-      describe "all" do
-        it "returns object list by the client" do
-          expect(described_class).to receive(:bucket){bucket}
-          collection = Aws::S3::ObjectSummary::Collection.new(nil)
-          binding.irb
-          expect(bucket).to receive(:objects).with(prefix: ENV['SHARED_CONFIG_PATH_ROOT']){collection}
-          expect(collection).to receive(:each)
-          objects = described_class.all
-        end
-      end
+    it "calls load on each item in SharedConfig::Group.all if nothing is passed in" do
+      expect(SharedConfig::Group).to receive(:all){[group]}
+      expect(group).to receive(:load)
+      described_class.load
     end
-
   end
 
+  describe SharedConfig::ObjectKeys do
+    describe "from_group_names" do
+      it "calls from_group_name for each item supplied" do
+        names = %w{some-name some-other}
+        expect(described_class).to receive(:from_group_name).with('some-name')
+        expect(described_class).to receive(:from_group_name).with('some-other')
+        described_class.from_group_names(names)
+
+        expect(described_class).to receive(:from_group_name).with('some-name')
+        expect(described_class).to receive(:from_group_name).with('some-other')
+        described_class.from_group_names(*names)
+      end
+    end
+    describe "from_group_name" do
+      it "combines the the name with ENV['SHARED_CONFIG_PATH_ROOT']" do
+        expect(described_class.from_group_name('some-group-name')).to eq("#{ENV['SHARED_CONFIG_PATH_ROOT']}/some-group-name.json")
+        expect(described_class.from_group_name('some-group-name.json')).to eq("#{ENV['SHARED_CONFIG_PATH_ROOT']}/some-group-name.json")
+      end
+    end
+  end
+  describe SharedConfig::EnvVar do
+    describe "set" do
+      it "sets an environment variable" do
+        key = rand_string
+        value = rand_string
+        described_class.set(key, value)
+        expect(ENV[key]).to eq(value)
+      end
+    end
+  end
+
+  describe SharedConfig::Group do
+    subject{described_class.new(object_key: rand_string, object_summary: object_summary)}
+
+    describe "load" do
+      it "calls EnvVar.set with item from load_json" do
+        expect(subject).to receive(:load_json).with(subject.object_summary){{"key"=>"value"}}
+        expect(SharedConfig::EnvVar).to receive(:set).with("key", "value")
+        subject.load
+      end
+    end
+
+    describe "load_json" do
+      let(:body){double("Body")}
+      let(:object){Aws::S3::Object.new(bucket_name: bucket.name, key: object_summary.key)}
+      it "returns a parsed version of the object's content" do
+        expect(object_summary).to receive(:get){object}
+        expect(object).to receive(:body){body}
+        expect(body).to receive(:read){"\[1\]"}
+        expect(subject.load_json(object_summary)).to eq([1])
+      end
+
+      it "does not blow up if the json is unparseable" do
+        expect(object_summary).to receive(:get){object}
+        expect(object).to receive(:body){body}
+        expect(body).to receive(:read){""}
+        expect(subject.load_json(object_summary)).to eq({})
+      end
+
+      it "does not blow up if the returned value is nil" do
+        expect(object_summary).to receive(:get){object}
+        expect(object).to receive(:body){body}
+        expect(body).to receive(:read){nil}
+        expect(subject.load_json(object_summary)).to eq({})
+      end
+    end
+
+    describe "load_object_summary" do
+      it "loads the object_summary wth the env bucket" do
+        expect(described_class).to receive(:bucket){bucket}
+        expect(Aws::S3::ObjectSummary).to receive(:new).with(bucket.name, subject.object_key){object_summary}
+        subject.load_object_summary
+        expect(subject.object_summary).to eq(object_summary)
+      end
+    end
+
+    describe "all_object_summaries" do
+      it "returns object_summary objects by the client" do
+        expect(described_class).to receive(:bucket){bucket}
+        collection = Aws::S3::ObjectSummary::Collection.new(nil)
+        expect(bucket).to receive(:objects).with(prefix: ENV['SHARED_CONFIG_PATH_ROOT']){collection}
+        expect(collection).to receive(:each)
+        object_summaries = described_class.all_object_summaries
+      end
+    end
+
+    describe "all" do
+      it "returns a Group for each item in all_object_summaries" do
+        expect(described_class).to receive(:all_object_summaries){[object_summary]}
+        groups = described_class.all
+        expect(groups.first.object_summary).to eq(object_summary)
+      end
+    end
+  end
   it "has a version number" do
     expect(SharedConfig::VERSION).not_to be nil
   end
